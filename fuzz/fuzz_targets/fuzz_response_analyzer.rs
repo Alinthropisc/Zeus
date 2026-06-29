3b3451e804986747f50baa74c2c052f86ee035f9
Ali <Sayavdera@protonmail.com>
2026-06-29 19:32:20 +0500

    updating

--- fuzz/fuzz_targets/fuzz_response_analyzer.rs
+++ fuzz/fuzz_targets/fuzz_response_analyzer.rs
@@ -1,10 +1,12 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
+use std::collections::HashMap;
 use zeus_core::response_analyzer::ResponseAnalyzer;
 
 fuzz_target!(|data: &[u8]| {
     if let Ok(s) = std::str::from_utf8(data) {
         let analyzer = ResponseAnalyzer::default();
-        let _ = analyzer.analyze(s);
+        let headers = HashMap::new();
+        let _ = analyzer.analyze(200, s, &headers, None);
     }
 });
