03c00062703943b60d5e4df7af30c882b71df420
Ali <Sayavdera@protonmail.com>
2026-06-29 19:20:54 +0500

    updating

--- fuzz/fuzz_targets/fuzz_credential_parser.rs
+++ fuzz/fuzz_targets/fuzz_credential_parser.rs
@@ -1,9 +1,12 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
-use zeus_attack::wordlist::parse_wordlist_line;
+use zeus_attack::wordlist::Wordlist;
 
 fuzz_target!(|data: &[u8]| {
     if let Ok(s) = std::str::from_utf8(data) {
-        let _ = parse_wordlist_line(s);
+        let entries: Vec<String> = s.lines().map(|l| l.to_string()).collect();
+        let wl = Wordlist::from_vec(entries);
+        let _ = wl.len();
+        let _ = wl.passwords().count();
     }
 });
